1d6e0829a8ab48e18b9fa7d8b870dd13c5bb54d0
zaidalbaik <zaidalbake@gmail.com>
2023-11-27 22:20:31 +0300

    Some Changes

--- AngularLearnProj/src/app/app.component.ts
+++ AngularLearnProj/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TestComponent } from './test/test.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TestComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- AngularLearnProj/src/app/test/test.component.ts
+++ AngularLearnProj/src/app/test/test.component.ts
@@ -1,13 +1,125 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Inject, PLATFORM_ID, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { isPlatformBrowser } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './test.component.html',
-  styleUrl: './test.component.css'
+  styleUrl: './test.component.css',
+  encapsulation: ViewEncapsulation.ShadowDom
 })
+
 export class TestComponent {
 
+  //get
+  public get platformId(): Object {
+    return this._platformId;
+  }
+  public name = "Hello";
+  public url = "";
+  public cssClass = "text-primary"
+  public isError = true;
+  public isSuccess = true;
+  public testText = 0;
+  public msgBindings!: string;
+  public textOneWay = 'default value';
+
+  fontSize = 16; // Initial font size
+
+  public titleStyle = {
+    color: "Orange",
+    backgroundColor: "black",
+    textDecoration: "underline",
+  }
+
+  public tstClass = {
+    "text-danger": !this.isSuccess,
+    "text-success": this.isSuccess
+  }
+
+  constructor(@Inject(PLATFORM_ID) private readonly _platformId: Object) {
+    this.name = "zaid";
+
+    if (isPlatformBrowser(this.platformId)) {
+      // Only access window object in a browser environment
+      this.url = window.location.href;
+
+    }
+  }
+  onClick(ev: object): void {
+    this.testText += 1;
+  }
+  onInputClick(val: string): void {
+    console.log(val);
+  }
+  keyup(value: string) {
+    this.textOneWay = value;
+  }
+  increaseFontSize() {
+    this.fontSize += 2;
+  }
+
+  decreaseFontSize() {
+    this.fontSize = Math.max(2, this.fontSize - 2);
+  }
 }
+
+
+// @Component({
+//   selector: 'app-ss',
+//   standalone: true,
+//   template: `<h1>Heloooooooooooooooooooooooooooooo</h1>`,
+//   imports: [CommonModule]
+// })
+// export class SComponent {
+//   title = 'AngularLearnProj';
+// }
+
+
+
+
+// // learn Type Script
+
+// interface Car {
+//   name: string;
+//   color: string;
+//   getColor(): string;
+//   getName(): string;
+// }
+
+// class Toyota implements Car {
+//   constructor(public name: string, public color: string) {
+//   }
+
+
+//   getColor(): string {
+//     return this.color;
+//   }
+//   getName(): string {
+//     return this.name;
+//   }
+// }
+
+// let c = new Toyota("toyota", "red");
+
+// console.log(c.color);
+// console.log(c.name);
+// console.log(c.getColor());
+
+// enum days {
+//   sun,
+//   mon,
+//   tus,
+//   wed,
+//   ther,
+//   fri,
+//   sat
+// }
+
+// // let arr = [days.fri, days.sat];
+// // arr.forEach(r => document.write(`
+// // <p class='rr'>${r}</p>
+// // `));
